7939cfcf2c8636fdbaa42f7f6e1775548195d908
Daniel Walton <dwalton76@gmail.com>
2018-11-17 10:15:04 -0500

    use jquery

--- include/index.js
+++ include/index.js
@@ -1,17 +1,35 @@
-document.onkeydown = function(e) {
-    e.preventDefault();
-    if (!e.repeat) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/send.key', true);
-        xhr.send(e.keyCode + ',1');
-    }
-}
 
-document.onkeyup = function(e) {
-    e.preventDefault();
-    if (!e.repeat) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/send.key', true);
-        xhr.send(e.keyCode + ',0');
+// Prevent the page from scrolling on an iphone
+// http://stackoverflow.com/questions/7768269/ipad-safari-disable-scrolling-and-bounce-effect
+$(document).bind(
+    'touchmove',
+    function(e) {
+        e.preventDefault();
     }
-}
+);
+
+
+$(document).ready(function() {
+
+    $(document).keydown(function(e) {
+        // console.log("keydown: " + e.keyCode)
+        e.preventDefault();
+
+        if (!e.repeat) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/send.key', true);
+            xhr.send(e.keyCode + ',1');
+        }
+    });
+
+    $(document).keyup(function(e) {
+        // console.log("keyup: " + e.keyCode)
+        e.preventDefault();
+
+        if (!e.repeat) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/send.key', true);
+            xhr.send(e.keyCode + ',0');
+        }
+    });
+});
